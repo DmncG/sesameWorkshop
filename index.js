01a527e7b0a63e930c554bf984933cb311c871b2
DmncG <drbgarcia389@yahoo.com>
2018-04-26 16:53:55 -0400

    feat add Big Bird

--- index.js
+++ index.js
@@ -21,4 +21,40 @@ box.addEventListener('click' , function () {
             box.style.left = pos + 'px'; 
           }
         }
+
+        //moving eyes
+        let eyeOne = document.getElementById('eye1');
+        let eyeTwo = document.getElementById('eye2');
+
+        let eyeOneStyle = window.getComputedStyle(eyeOne);
+        let eyeTwoStyle = window.getComputedStyle(eyeTwo);
+
+        let eyeOneLeft = eyeOneStyle.getPropertyValue('left');
+        let eyeOneTop = eyeOneStyle.getPropertyValue('top');
+
+        let eyeTwoLeft = eyeTwoStyle.getPropertyValue('left');
+
+        let eyeId = setInterval(eyeFrame, 100);
+
+        let oneLeftVal = Number(eyeOneLeft.match(/[0-9]/gi).join(''));
+        let oneTopVal = Number(eyeOneTop.match(/[0-9]/gi).join(''));
+
+        let twoLeftVal = Number(eyeTwoLeft.match(/[0-9]/gi).join(''));
+
+        function eyeFrame(){
+            console.log(oneTopVal)
+            if(oneLeftVal === 300){
+                clearInterval(eyeId)
+            }
+            else{
+            if(oneTopVal !== 215){
+                oneTopVal--;
+                eyeOne.style.top = oneTopVal + 'px';
+            }
+            oneLeftVal++;
+            twoLeftVal++;
+            eyeOne.style.left = oneLeftVal + 'px';
+            eyeTwo.style.left = twoLeftVal + 'px';
+            }
+        }
 })
